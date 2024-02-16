9995fd1edb1341daa6ec70a0175642bab8793b9a
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-02-16 19:40:24 +0000

    added links

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/klaudia-portfolio-react/",
+  base: "/Klaudia-portfolio-react/",
   plugins: [react()],
   build: {
     outDir: 'dist'
