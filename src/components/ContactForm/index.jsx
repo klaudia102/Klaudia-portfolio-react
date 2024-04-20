cf0a68520b4b3e51d4eb990593b02e5ab6a5e7b4
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-04-20 16:46:58 +0100

    change to contact form

---
+++ src/components/ContactForm/index.jsx
@@ -0,0 +1,45 @@
+import './style.css'
+import React from 'react'
+import emailjs from 'emailjs-com';
+// import emailjs from 'emailjs/browser'
+
+function ContactForm() {
+
+    const SERVICE_ID = "service_73giv99";
+    const TEMPLATE_ID = "template_4mtjolb";
+    const PUBLIC_KEY = "SglSW3ofcWnqBYNB3";
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
+            .then((result) => {
+                alert('Message gas been sent!')
+            }, (error) => {
+                console.log(error.text);
+                alert('Something gone wrong!')
+            });
+        e.target.reset()
+    };
+    return (
+        <form class="formContainer form" onSubmit={handleOnSubmit}>
+            <div className="mb-3 container-name">
+                <label for="exampleFormControlInput1 from_name" className="form-label">name</label>
+                <input type="text" name="from_name" className="form-control email-input" id="exampleFormControlInput1 from_name" required />
+
+            </div>
+            <div className="mb-3 container-email">
+                <label for="exampleFormControlInput1 from_email" className="form-label">email</label>
+                <input type="email" name="from_name" className="form-control email-input" id="exampleFormControlInput1 from_email" required />
+            </div>
+            <div class="mb-3 container-massage" >
+                <label for="exampleFormControlTextarea1 message" className="form-label">message</label> <br />
+                <textarea name="message" className="form-control" id="exampleFormControlTextarea1" required></textarea>
+            </div>
+            <div className='button-container'>
+                <button type="button submitt" className="btn sent_button">sent</button>
+            </div>
+        </form>
+    )
+}
+
+export default ContactForm;
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
 import './contact.css'
+import React from 'react';
+import ContacForm from '..//../components/ContactForm/index'
+
 
 
 function Contact() {
@@ -18,44 +19,9 @@ function Contact() {
             <a href='./Klaudia Kierepka-CV.pdf' target='blank' className='link'>Download my CV</a>
           </div>
         </div>
-        {/* contact input */}
-        <div className='contact-form-container'>
-          <form nameClass="row g-3 needs-validation " novalidate>
-
-            <div nameClass="col-md-4 position-relative contact-form input1">
-              <input type="text" nameClass="form-control" id="validationTooltip01" placeholder="Name" required />
-              <div nameClass="valid-tooltip">
-              </div>
-            </div>
-            <div nameClass="col-md-4 position-relative contact-form input1">
-              <input type="text" nameClass="form-control" id="validationTooltip02" placeholder="Surname" required />
-              <div nameClass="valid-tooltip">
-              </div>
-            </div>
-
-            <div nameClass="col-md-4 position-relative contact-form input1">
-              <div nameClass="input-group has-validation">
-                <span nameClass="input-group-text" id="validationTooltipUsernamePrepend"></span>
-                <input type="text" nameClass="form-control"
-                  id="validationTooltipUsername" aria-describedby="validationTooltipUsernamePrepend"
-                  placeholder="Email" required />
-                <div nameClass="invalid-tooltip">
-                </div>
-              </div>
-
-            </div>
-            <form nameClass="was-validated">
-              <div nameClass="mb-3">
-                <textarea nameClass="form-control" id="validationTextarea" placeholder="Required example textarea" required></textarea>
-                <div nameClass="invalid-feedback">
-                </div>
-              </div>
-            </form>
-            <div nameClass="col-12 button-container">
-              <button className="btn btn-primary" type="submit">Submit</button>
-            </div>
-          </form>
-        </div>
+        
+        <ContacForm />
+        
       </div>
     </>
   );
